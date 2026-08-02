f58df8fffa3bc29fd3aabfe1985dc8f64948d831
manjitrana <manjitrana621@gmail.com>
2026-08-03 01:57:54 +0530

    Rename the property in your model to the singular form

--- app/src/main/java/com/autonomousone/messages/model/Sms.kt
+++ app/src/main/java/com/autonomousone/messages/model/Sms.kt
@@ -3,7 +3,7 @@ package com.autonomousone.messages.model
 data class Sms(
     val id: Long,
     val sender: String,
-    val messages: String,
+    val message: String,
     val time: String,
     val unread: Boolean = false
 )
