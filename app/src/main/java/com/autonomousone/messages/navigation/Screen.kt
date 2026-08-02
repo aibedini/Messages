48edefa9febd5c1f4546dee046ae2550122d881c
manjitrana <manjitrana621@gmail.com>
2026-08-03 02:12:56 +0530

    Created sealed class for routes

--- app/src/main/java/com/autonomousone/messages/navigation/Screen.kt
+++ app/src/main/java/com/autonomousone/messages/navigation/Screen.kt
@@ -1,2 +1,13 @@
 package com.autonomousone.messages.navigation
 
+sealed class Screen(val route: String) {
+
+    object Home : Screen("home")
+
+    object Conversation : Screen("conversation/{sender}") {
+
+        fun createRoute(sender: String): String {
+            return "conversation/$sender"
+        }
+    }
+}
