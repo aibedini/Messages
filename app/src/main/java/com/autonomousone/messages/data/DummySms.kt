ce19a18d4fa44a2602482a2516231438d4e98b9d
manjitrana <manjitrana621@gmail.com>
2026-08-03 01:36:36 +0530

    Added Dummy Sms Data

---
+++ app/src/main/java/com/autonomousone/messages/data/DummySms.kt
@@ -0,0 +1,40 @@
+package com.autonomousone.messages.data
+
+import com.autonomousone.messages.model.Sms
+
+val dummySms = listOf(
+    Sms(
+        1,
+        "Manjit Rana",
+        "Hey! Are You Coming today ?",
+        "10:35 PM",
+        true
+    ),
+    Sms(
+        2,
+        "Amazon",
+        "Your order has been shipped.",
+        "Yesterday"
+    ),
+
+    Sms(
+        3,
+        "ICICI Bank",
+        "OTP is 458912",
+        "Yesterday"
+    ),
+
+    Sms(
+        4,
+        "Rahul",
+        "Let's meet tomorrow.",
+        "Jul 30"
+    ),
+
+    Sms(
+        5,
+        "Swiggy",
+        "Your food is arriving in 8 minutes.",
+        "Jul 29"
+    )
+)
