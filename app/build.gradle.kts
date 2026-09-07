0fc725ea318d1338c110b7a7823a6f7957499f7c
aibedini <aibedini@users.noreply.github.com>
2026-09-07 12:31:53 +0330

    Release-Android-2.8.6-telemetry

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.autonomousone.messages"
         minSdk = 26
         targetSdk = 36
-        versionCode = 90
-        versionName = "2.8.5"
+        versionCode = 91
+        versionName = "2.8.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
