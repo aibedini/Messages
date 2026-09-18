c10aff7a6b4230c08196462ac828e6c59d625518
aibedini <aibedini@users.noreply.github.com>
2026-09-18 22:38:05 +0330

    release: prepare v3.3.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.autonomousone.messages"
         minSdk = 26
         targetSdk = 36
-        versionCode = 104
-        versionName = "3.3.4"
+        versionCode = 105
+        versionName = "3.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
